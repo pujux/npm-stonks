d2edd7eec5e09e20bf3c06cd1df1706b9ae72681
Julian Pufler <puf17640@spengergasse.at>
2020-06-29 12:19:01 +0200

    added progress bar

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 #!/usr/bin/env node
 (async () => {
 	const request = require('@aero/centra')
-		sleep = require('sleep-async')
+		sleep = require('sleep-async'),
+		cliProgress = require('cli-progress')
+		
+	const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
 	const [,, mod, count] = process.argv
 	if (!mod || !count) return
-	console.log(`adding ${count} downloads to '${mod}'`)
+	console.log(`Adding ${count} downloads to '${mod}'.\n`)
 	const data = await request(`https://registry.npmjs.org/${mod}`).json()
-	if (data.error) return
+	if (data.error)	return console.error(`Could not find module '${mod}'.`)
 	const tarballUrl = data.versions[data['dist-tags'].latest].dist.tarball
+	progress.start(count, 0)
 	for (let counter = 1; counter <= count;) {
-		if ((await request(tarballUrl).send()).statusCode === 200) counter++
-		await sleep(1000)
+		if ((await request(tarballUrl).send()).statusCode === 200) {
+			progress.update(counter)
+			counter++
+			await sleep(1000)
+		}
 	}
-	console.log(`finished`)
+	progress.stop()
+	console.log(`\nDone!`)
 })()
