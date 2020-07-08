549b0f3cdccb784e4e7b55fe0cbcf6809831585f
Julian Pufler <puf17640@spengergasse.at>
2020-07-08 11:22:36 +0200

    timeout arg

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 #!/usr/bin/env node
 (async () => {
-	const request = require('@aero/centra'),
-		sleep = require('sleep-async'),
-		cliProgress = require('cli-progress')
-	const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
-	const [,, mod, count] = process.argv
-	if (!mod || !count) return console.log('You need to specify a module and a download count.')
-	const data = await request(`https://registry.npmjs.org/${mod}`).json()
-	if (data.error)	return console.error(`Could not find module '${mod}'.`)
-	else console.log(`Adding ${count} downloads to '${mod}'.\n`)
-	const tarballUrl = data.versions[data['dist-tags'].latest].dist.tarball
-	progress.start(count, 0)
-	for (let counter = 1; counter <= count;) {
-		if ((await request(tarballUrl).send()).statusCode === 200) {
-			progress.update(counter)
-			counter++
-			await sleep(1000)
+	try {
+		const request = require('@aero/centra'),
+			sleep = require('sleep-async')().Promise.sleep,
+			cliProgress = require('cli-progress')
+		const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+		const [,, mod, count, timeout=100] = process.argv
+		if (!mod || !count) return console.log('You need to specify a module and a download count.')
+		const data = await request(`https://registry.npmjs.org/${mod}`).json()
+		if (data.error)	return console.error(`Could not find module '${mod}'.`)
+		else console.log(`Adding ${count} downloads to '${mod}' using a ${timeout}ms timeout.\n`)
+		const tarballUrl = data.versions[data['dist-tags'].latest].dist.tarball
+		progress.start(count, 0)
+		for (let counter = 1; counter <= count;) {
+			if ((await request(tarballUrl).send()).statusCode === 200) {
+				progress.update(++counter)
+				await sleep(parseInt(timeout))
+			}
 		}
+		progress.stop()
+		console.log(`\nDone!\nhttps://npmjs.com/${mod}`)
+	}catch{
+		console.error('An unknown error occured. Please retry.')
 	}
-	progress.stop()
-	console.log(`\nDone!\nhttps://npmjs.com/${mod}`)
 })()
